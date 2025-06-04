e74d8383831373474e600abe91cc2076fde24077
AATHEESWARANE <aatheeswarane3@gmail.com>
2025-06-04 12:41:59 +0530

    final

---
+++ collections.js
@@ -0,0 +1,22 @@
+import {products} from "./products.js";
+
+
+var offerBar = document.querySelector(".offer-bar")
+
+document.getElementById("offer-close").addEventListener("click",
+
+function(){
+    offerBar.style.display="none"
+}
+)
+
+var sideNavMenu=document.querySelector(".navbar-menu-toggle")
+var sidenavbar = document.querySelector(".side-navbar")
+sideNavMenu.addEventListener("click",function(){
+   
+    sidenavbar.style.marginLeft="0px"
+})
+
+document.getElementById("side-navbar-close").addEventListener("click",()=>{
+    document.querySelector(".side-navbar").style.marginLeft = "-60%"
+})
